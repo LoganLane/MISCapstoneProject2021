361a6923cdd0349a299b27c3a1d790b641631506
SOwens139 <70295606+SOwens139@users.noreply.github.com>
2021-04-16 13:39:49 -0400

    Update TweetFilter.py

--- TweetFilter.py
+++ TweetFilter.py
@@ -15,14 +15,13 @@ with open("twitter_credentials.json", "r") as file:
     
 twitter = Twython(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'], creds['ACCESS_TOKEN'], creds['ACCESS_SECRET'])
     
-results = twitter.search(q= 'hate -filter:retweets', tweet_mode ='extended', count= 100, lang= 'en')
+for x in range(1):
+    results = twitter.search(q= 'hate -filter:retweets', tweet_mode ='extended', count= 100, lang= 'en')
+    all_tweets = results['statuses']
 
-all_tweets = results['statuses']
-
-for tweet in all_tweets:
+    for tweet in all_tweets:
    
-    with open(r'saved_tweets4.csv', 'a',encoding = 'utf-8') as file:
+        with open(r'Test.csv', 'a',encoding = 'utf-8') as file:
             writer = csv.writer(file)
             writer.writerow(list([tweet['full_text']]))
         
- 
